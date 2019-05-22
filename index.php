b9e2bc631436fefd0285ba195ccae1c6d6eee229
Bora DAN <monera.h@hotmail.com>
2019-05-22 16:36:32 +0300

    documantation added

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta http-equiv="refresh" content="0;URL=http://raxus.pikselmatik.com/index.html" />
+
+<script src="http://demos.pikselmatik.com/assets/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script src="http://demos.pikselmatik.com/assets/js/custom-jquery.js" type="text/javascript"></script>
+
+</head>
+<body>
+
+</body>
+</html>
