2d8cd5b4dad1586b72c634b233553770cd83ab31
Bora DAN <monera.h@hotmail.com>
2019-05-22 16:59:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 
-<meta http-equiv="refresh" content="0;URL=http://raxus.pikselmatik.com/index.html" />
+<meta http-equiv="refresh" content="0;URL=https://bqra.github.io/Raxus-Slider-Doc/" />
 
 <script src="http://demos.pikselmatik.com/assets/js/jquery-1.9.1.min.js" type="text/javascript"></script>
 <script src="http://demos.pikselmatik.com/assets/js/custom-jquery.js" type="text/javascript"></script>
